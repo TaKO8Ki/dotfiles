a55f5487e94931f7f4334ce3f4c3049ed96f44d3
TaKO8Ki <takoyaki0316@gmail.com>
2019-11-02 17:35:20 +0900

    コメント追加

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install packages and apps
 brew bundle
 
 set -u
